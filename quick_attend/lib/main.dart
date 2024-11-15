8e1332f76c25ec8e1e4d64780c6fba00fef7c880
Aryanlanghanoja <aryanlanghanoja233@gmail.com>
2024-11-15 08:55:56 +0530

    Added A Project

---
+++ quick_attend/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:quick_attend/firebase_options.dart';
+import 'package:quick_attend/screens/home_screen.dart';
+import 'package:quick_attend/screens/splash_screen.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quick Attend',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+          routes: {
+        '/home': (context) => const Homescreen(), // Define your home screen here
+      },
+    );
+  }
+}
---
+++ quick_attend/lib/screens/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(
+          context, '/home'); // Navigate to home screen
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Set background color to white
+      body: Center(
+        child: Image.asset('assets/Images/Logo.jpg'), // Display splash image
+      ),
+    );
+  }
+}
